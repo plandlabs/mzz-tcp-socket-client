5538bd79da57ab60746920188e4c0f11660c16e6
plandlabs <106055408+plandlabs@users.noreply.github.com>
2022-11-25 19:08:20 +0900

    woojin commit

--- client.js
+++ client.js
@@ -1,97 +1,98 @@
-const net = require('net');
-const express = require('express');
+const net = require("net");
+const express = require("express");
 const app = express();
-require('date-util');
-import EventEmitter from 'events';
-let hostPort = 33300;
-let hostIP = '192.168.219.200';
+let emitter = require("events");
+require("date-util");
+let hostport = 33300;
+let hostip = "192.168.219.200";
 var clientUser;
 
-class Emitter extends EventEmitter { }
-const emitter = new Emitter();
-emitter.setMaxListeners(15);
+function getConnection(connName) {
+	var client = net.connect({ port: hostport, host: hostip }, function () {
+		this.on("connect", function () {
+			console.log(connName + " connected!");
+		});
 
-function getConnection(connName){
-  var client = net.connect({port: hostPort, host: hostIP}, function() {
-    
-    this.on('connect', function() {
-      // var jsonToData = JSON.parse(JSON.stringify(Buffer.from(data, 'base64'))).data;
-      console.log(connName + " connected!");
-    });
-    
-    this.on('send', function() {
-      // var jsonToData = JSON.parse(JSON.stringify(Buffer.from(data, 'base64'))).data;
-      console.log(connName + " message");
-    });
+		this.on("data", function (data) {
+			console.log("받았어 : " + data + " / " + new Date().format("yyyy-mm-dd hh:mm:ss"));
+      		//client.end();
+    	});
 
-    this.on('data', function(data) {
-      // var jsonToData = JSON.parse(JSON.stringify(Buffer.from(data, 'base64'))).data;
-      console.log("받았어 : " + data + " / " + new Date().format("yyyy-mm-dd HH:MM:ss"));
-      //client.end();
-    });
+		this.on("end", function () {
+			console.log(connName + " disconnected!");
+		});
 
-    this.on('end', function() {
-      console.log(connName + ' disconnected!');
-    });
+		this.on("error", function (err) {
+			console.log("error : ", JSON.stringify(err));
+		});
 
-    this.on('error', function(err) {
-      console.log('error : ', JSON.stringify(err));
-    });
+		this.on("timeout", function () {
+			console.log("소켓 타임아웃");
+		});
 
-    this.on('timeout', function() {
-      console.log('소켓 타임아웃');
-    });
+		this.on("close", function () {
+			console.log("소켓 닫힘");
+			client.reconnect(5 * 1000);
+		});
 
-    this.on('close', function() {
-      console.log('소켓 닫힘');
-      client.reconnect(5 * 1000);
-    });
-
-    this.reconnect = interval =>{
-        setTimeout(() => {
-            console.log(connName + ' 서버와 재연결 중');
-            client.connect(hostPort, hostIP)
-        }, interval)
-    }
-
-  });
-  return client;
-}
- 
-function writeData(socket, data){
-  var success = !socket.write(data);
-  console.log(socket.listenerCount('connect'));
-  if (!success){
-    (function(socket, data){
-      socket.once('drain', function(){
-        writeData(socket, data);
-        socket.end();
-      });
-    })(socket, data);
-  }
+		this.reconnect = (interval) => {
+			setTimeout(() => {
+				console.log(connName + " 서버와 재연결 중");
+				client.connect(hostport, hostip);
+			}, interval);
+		};
+  	});
+  	return client;
 }
 
-var moveRobotTime = function(){
-  let data = "음오아예";
-  let jsonArr = {
-    a1 : "123"
-    ,a2 : "456"
-    ,a3 : "789"
-    ,a4 : new Date().format("yyyy-mm-dd HH:MM:ss")
-  };
-  writeData(clientUser, data);
-  console.log('보냈어 : ' + new Date().format("yyyy-mm-dd HH:MM:ss"));
-}
-//setInterval(moveRobotTime, 1000 * 1);
+emitter.setMaxListeners(5);
 
-app.get('/', (req, res) =>{
-  clientUser = getConnection("robo3");
-  //moveRobotTime();
-  setInterval(moveRobotTime, 1000 * 1);
-  res.send('client open!');
-  console.log("client open!");
+function writeData(socket, data) {
+	var success = !socket.write(data);
+	if (!success) {
+		(function (socket, data) {
+			socket.on("drain", function () {
+				console.log("!success");
+				writeData(socket, data);
+				socket.end();
+			});
+		})(socket, data);
+	}
+}
+
+var moveRobotTime = function () {
+	let data = "음오아예";
+	let jsonarr = {
+		a1: "123",
+		a2: "456",
+		a3: "789",
+		a4: new Date().format("yyyy-mm-dd HH:MM:ss"),
+	};
+	writeData(clientUser, data);
+	console.log(clientUser.listenerCount("drain"));
+	if (clientUser.listenerCount("drain") > 0) {
+		clientUser.removeAllListeners("drain");
+	}
+	console.log("보냈어 : " + new Date().format("yyyy-mm-dd HH:MM:ss"));
+};
+app.get("/", (req, res) => {
+	res.send("client open!");
+	console.log("client open!");
+});
+app.get("/viewer", (req, res) => {
+	clientUser = getConnection("viewer");
+	res.send("viewer open!");
+	console.log("viewer open!");
 });
 
-const server = app.listen(33080,  () => {
-  console.log(__dirname);
+app.get("/robot", (req, res) => {
+	clientUser = getConnection("robot");
+	//moveRobotTime();
+	setInterval(moveRobotTime, 1000 * 5);
+	res.send("robot open!");
+	console.log("robot open!");
+});
+
+const server = app.listen(33080, () => {
+  	console.log(__dirname);
 });
